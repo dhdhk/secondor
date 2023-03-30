ea59a099ba8bae31c9abf4ab1c2b2c03af06f2ed
dhdhg <dhdhg@DAHEE>
2023-03-30 12:17:27 +0900

    ddgddd

--- secondProject/src/main/java/com/spring/second/write/controller/WriteControllerImpl.java
+++ secondProject/src/main/java/com/spring/second/write/controller/WriteControllerImpl.java
@@ -3,15 +3,22 @@ package com.spring.second.write.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class WriteControllerImpl implements WriteController{
 
 	@Override
-	@RequestMapping("/write.do")
-	public String write(HttpServletRequest request, HttpServletResponse response) throws Exception {
+	@RequestMapping(value = "/write/writeForm.do")
+	public ModelAndView write(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
-		return "write";
+		String viewName = (String) request.getAttribute("viewName");
+		ModelAndView mav = new ModelAndView(viewName);
+		
+		return mav;
 	}
 
 }
