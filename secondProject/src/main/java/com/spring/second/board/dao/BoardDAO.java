8bb3f205b1cbcd09e1cb77861d0cf800db3e7e6f
mamma <mamma@DESKTOP-N94SCU9>
2023-04-06 11:05:59 +0900

    main.java

--- secondProject/src/main/java/com/spring/second/board/dao/BoardDAO.java
+++ secondProject/src/main/java/com/spring/second/board/dao/BoardDAO.java
@@ -1,11 +1,21 @@
 package com.spring.second.board.dao;
 
 import java.util.List;
+import java.util.Map;
+
+import org.springframework.dao.DataAccessException;
 
 import com.spring.second.board.dto.BoardDTO;
 
 public interface BoardDAO {
 
-   List<BoardDTO> selectAllArticlesList();
+	List<BoardDTO> selectAllArticlesList() throws DataAccessException;
 
+	List<BoardDTO> selectArticlesByCategory(String category_name)throws DataAccessException;
+
+	public int count();
+
+	public List<BoardDTO> selectPage(Map map);
+	
+	public Map<String, Object> viewProduct(int regNum);
 }
--- secondProject/src/main/java/com/spring/second/board/service/BoardService.java
+++ secondProject/src/main/java/com/spring/second/board/service/BoardService.java
@@ -1,10 +1,18 @@
 package com.spring.second.board.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.spring.second.board.dto.BoardDTO;
 
 public interface BoardService {
-   List<BoardDTO> listArticles();
+	public List<BoardDTO> listArticles();
 
+	public List<BoardDTO> listArticlesByCategory(String category_name);
+
+	public int getCount();
+
+	public List<BoardDTO> getPage(Map map);
+	
+	public Map<String, Object> viewProduct(int regNum);
 }
