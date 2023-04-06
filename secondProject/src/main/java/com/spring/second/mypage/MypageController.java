0fdef8be5c41805f398c52a98bb0e9765b6ec8c7
dhdhg <dhdhg@DAHEE>
2023-04-06 11:07:14 +0900

    dh

---
+++ secondProject/src/main/java/com/spring/second/mypage/MypageController.java
@@ -0,0 +1,21 @@
+package com.spring.second.mypage;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class MypageController {
+
+	@RequestMapping(value = "/mypage/mypageMain.do")
+	public ModelAndView write(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		String viewName = (String) request.getAttribute("viewName");
+		ModelAndView mav = new ModelAndView(viewName);
+		
+		return mav;
+	}
+}
