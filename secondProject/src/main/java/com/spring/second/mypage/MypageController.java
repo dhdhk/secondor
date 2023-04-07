3099e7670688c41ee1e235bb5dacd62c19ef00af
dhdhg <dhdhg@DAHEE>
2023-04-07 10:21:18 +0900

    dhdh

--- secondProject/src/main/java/com/spring/second/mypage/MypageController.java
+++ secondProject/src/main/java/com/spring/second/mypage/MypageController.java
@@ -1,9 +1,5 @@
 package com.spring.second.mypage;
 
-
-
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,7 +9,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.spring.second.board.dto.BoardDTO;
 
 
 
@@ -29,31 +24,5 @@ public class MypageController {
 		ModelAndView mav = new ModelAndView(viewName);
 		
 		return mav;
-	}
-
-	
-	@RequestMapping(value = "/board/viewArticle.do")
-	public ModelAndView viewArticle(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// TODO Auto-generated method stub
-		String viewName = (String) request.getAttribute("viewName");
-		List<BoardDTO> boardList = viewArticles();
-		ModelAndView mav = new ModelAndView(viewName);
-		 mav.addObject("boardList", boardList);
-		return mav;
-	}
-
-
-	private List<BoardDTO> viewArticles() {
-		// TODO Auto-generated method stub
-		List<BoardDTO> boardList = selectviewArticle();
-	      return boardList;
-	}
-
-
-	private List<BoardDTO> selectviewArticle() {
-		// TODO Auto-generated method stub
-		List<BoardDTO> boardList = sqlSession.selectList("mapper.board.selectviewArticle");
-	      
-	      return boardList;
 	}
 }
