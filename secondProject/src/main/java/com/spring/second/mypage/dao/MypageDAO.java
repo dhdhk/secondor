eecc649fdb0d1136c5075c32cff7ff06cb67e77b
dhdhg <dhdhg@DAHEE>
2023-04-15 23:50:21 +0900

    프로필사진 java , mapper 수정

--- secondProject/src/main/java/com/spring/second/mypage/dao/MypageDAO.java
+++ secondProject/src/main/java/com/spring/second/mypage/dao/MypageDAO.java
@@ -1,13 +1,15 @@
 package com.spring.second.mypage.dao;
 
 import java.util.List;
+import java.util.Map;
 
 import com.spring.second.board.dto.BoardDTO;
 import com.spring.second.member.dto.MemberDTO;
 import com.spring.second.mypage.dto.MyproductlistPage;
 
 public interface MypageDAO {
-	public void updateInfo(MemberDTO memberDTO);
+	public void updateInfo(Map<String, Object> memberMap);
 	int ProductListCount(MyproductlistPage mp);
 	List<BoardDTO> selectMyList(MyproductlistPage mp);
+	public void updateInfoNoimg(Map<String, Object> memberMap);
 }
--- secondProject/src/main/java/com/spring/second/mypage/dao/MypageDAOImpl.java
+++ secondProject/src/main/java/com/spring/second/mypage/dao/MypageDAOImpl.java
@@ -16,19 +16,6 @@ public class MypageDAOImpl implements MypageDAO{
 	@Autowired
 	SqlSession sqlSession;
 
-	public void updateInfo(MemberDTO memberDTO) {
-		// TODO Auto-generated method stub
-
-		sqlSession.update("mapper.member.updateMember", memberDTO);
-	}
-
-	/*public List<BoardDTO> selectMyList(String id) {
-		// TODO Auto-generated method stub
-		List<BoardDTO> dto = sqlSession.selectList("mapper.board.selectMyList",id);
-		return dto;
-	}*/
-
-	//내상품 페이지 (총 상품 갯수)
 	public  int ProductListCount(MyproductlistPage mp) {
 		// TODO Auto-generated method stub
 		return sqlSession.selectOne("mapper.board.MyProductListCount",mp);
@@ -37,6 +24,18 @@ public class MypageDAOImpl implements MypageDAO{
 	public  List<BoardDTO> selectMyList(MyproductlistPage mp) {
 		// TODO Auto-generated method stub
 		return sqlSession.selectList("mapper.board.selectMyList",mp);
+	}
+
+	@Override
+	public void updateInfo(Map<String, Object> memberMap) {
+		// TODO Auto-generated method stub
+		sqlSession.update("mapper.member.updateMember", memberMap);
+	}
+
+	@Override
+	public void updateInfoNoimg(Map<String, Object> memberMap) {
+		// TODO Auto-generated method stub
+		sqlSession.update("mapper.member.updateMemberNoimg", memberMap);
 	}
 
 }
